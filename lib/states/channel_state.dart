a9d4a00661c92e3e5646855c950ec23bc3484067
mahfuzischo <mahfuzischo@gmail.com>
2026-01-22 18:59:27 +0600

    hehehe

--- lib/states/channel_state.dart
+++ lib/states/channel_state.dart
@@ -4,17 +4,25 @@ class ChannelState {
   final List<ChannelModel>? channels;
   final bool isLoading;
   final String? error;
-  ChannelState({this.channels, this.isLoading = false, this.error});
+  final bool maxFetched;
+  ChannelState({
+    this.channels,
+    this.isLoading = false,
+    this.error,
+    this.maxFetched = false,
+  });
 
   ChannelState copyWith({
     List<ChannelModel>? channelList,
     bool? loadingState,
     String? err,
+    bool? maxScrolled,
   }) {
     return ChannelState(
       channels: channelList ?? channels,
       isLoading: loadingState ?? isLoading,
       error: err ?? error,
+      maxFetched: maxScrolled ?? maxFetched,
     );
   }
 }
