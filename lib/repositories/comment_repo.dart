49c133dba83b300103c03bdbeb2d3899c760c22a
mahfuzischo <mahfuzischo@gmail.com>
2026-02-15 18:50:15 +0600

    comment widget2

---
+++ lib/repositories/comment_repo.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:demo_app/models/comment_model.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+class CommentRepo {
+  Future<List<CommentModel>> getComments(int feedId) async {
+    final String endpoint = '/student/comment/getComment/$feedId';
+    final url = Uri.parse('${dotenv.env['base_url']}$endpoint');
+    // final token = _storage  .readToken();
+
+    final response = await http.get(
+      url,
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': 'bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final tempData = jsonDecode(source)(response.body) as List;
+      final data = tempData.map((m) => CommentModel.fromJSON(m)).toList();
+      return data;
+    } else {
+      throw Exception(
+        'Failed to load comments with status code: ${response.statusCode}',
+      );
+    }
+  }
+}
